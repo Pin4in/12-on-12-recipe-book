5720a5ac6191eaa362863525534836e5adec048e
Pin4in <sergii.korkh@gmail.com>
2016-08-05 00:13:46 +0300

    submit recipe

---
+++ client/components/submit-recipe/submitRecipe.js
@@ -0,0 +1,18 @@
+// Template.submitRecipe.events({
+//   'submit .new-recipe': function(e) {
+//     e.preventDefault();
+//
+//     const newRecipeForm = e.target;
+//     console.log(newRecipeForm)
+//     const formData = {
+//       title: newRecipeForm.title.value,
+//       description: newRecipeForm.description.value,
+//       submitted: newRecipeForm.createdAt.value,
+//     };
+//
+//     console.log(formData);
+//
+//     Recipes.insert(formData);
+//
+//   }
+// });
--- collections/recipes.js
+++ collections/recipes.js
@@ -1,39 +1,55 @@
 Recipes = new Mongo.Collection("recipes");
 
 recipesSchema = new SimpleSchema({
-    title: {
-        type: String,
-        label: 'Price'
+  title: {
+    type: String,
+    label: 'Title'
+  },
+  description: {
+    type: String,
+    label: 'description'
+  },
+  categories: {
+    type: String,
+    label: 'categories'
+  },
+  price: {
+    type: Number,
+    label: 'Price'
+  },
+  inMenu: {
+    type: Boolean,
+    defaultValue: false,
+    optional: true,
+  },
+  author: {
+    type: String,
+    label: "Author",
+    autoValue: function() {
+      return this.userId
     },
-    description: {
-        type: String,
-        label: 'description'
-    },
-    categories: {
-        type: String,
-        label: 'categories'
-    },
-    price: {
-        type: Number,
-        label: 'Price'
-    },
-    inMenu: {
-        type: Boolean,
-        defaultValue: false,
-        optional: true,
-    },
-    author: {
-        type: String,
-        label: "Author",
-        autoValue: function() {
-            return this.userId
-        }
-    },
-    submitted: {
-      type: Date,
-      label: "Created At",
-      autoValue: function() {
-        return new Date()
-      },
+    autoform: {
+      type: "hidden"
     }
+  },
+  submitted: {
+    type: Date,
+    label: "Created At",
+    autoValue: function() {
+      return new Date()
+    },
+    autoform: {
+      type: "hidden"
+    }
+  }
+});
+
+Recipes.attachSchema(recipesSchema);
+
+
+Recipes.allow({
+  insert: function(userId, doc) {
+    // разрешить постить только если пользователь залогинен
+    return !! userId;
+  }
 });
